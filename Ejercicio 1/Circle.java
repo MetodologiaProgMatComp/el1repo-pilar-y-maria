9c1cc2f8e01e4dc5e5aebf40add72b9924d08f8b
mariadelaorden <mariadelaorden2005@hotmail.com>
2024-02-14 08:45:22 +0100

    Carpeta

--- Ejercicio 1/Circle.java
+++ Ejercicio 1/Circle.java
@@ -1,3 +1,5 @@
+package Ejercicio
+
 public class Circle { // Save as "Circle.java"
     // private instance variable, not accessible from outside this class
     private double radius;
---
+++ Ejercicio 1/Main.java
@@ -0,0 +1,7 @@
+package Ejercicio
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
