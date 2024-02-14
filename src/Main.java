4025f6ba7913e74b64bc653323eb5b4d1e5d1e72
pilarpadrino <pilar.padrino@edu.uah.es>
2024-02-14 09:50:08 +0100

    cambios ejemplo

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+
+
+import es.uah.matcomp.el1.circulo.Circle;
+
+
+public class Main { // Save as "TestCircle.java"
+
+    static public void main(String[] s) {
+        Circle c1 = new Circle();
+        // Invoke public methods on instance c1, via dot operator.
+        System.out.println("The circle has radius of "
+                + c1.getRadius() + " and area of " + c1.getArea());
+
+        Circle c2 = new Circle(2.0);
+
+        System.out.println("The circle has radius of "
+                + c2.getRadius() + " and area of " + c2.getArea());
+
+        Circle c3 = new Circle();
+        c3.setRadius(3.0);
+        System.out.println("radius is: " + c3.getRadius());
+        c3.setColor("blue");
+        System.out.println("color is: " + c3.getColor());
+    }
+}
---
+++ src/es/uah/matcomp/el1/circulo/Circle.java
@@ -0,0 +1,56 @@
+package es.uah.matcomp.el1.circulo;
+
+public class Circle { // Save as "Circle.java"
+    // private instance variables, not accessible from outside this class
+    private double radius;
+    private String color;
+
+    // Constructors (overloaded)
+    /** Constructs a Circle instance with default values for radius and color */
+    public Circle() { // 1st (default) constructor
+        radius = 1.0;
+        color = "red";
+    }
+
+    /** Constructs a Circle instance with the given radius and default color */
+    public Circle(double r) { // 2nd constructor
+        radius = r;
+        color = "red";
+    }
+
+    /** Constructs a Circle instance with the given radius and color */
+    public Circle(double r, String c) { // 3rd constructor
+        radius = r;
+        color = c;
+    }
+
+    /** Returns the radius */
+    public double getRadius() {
+        return radius;
+    }
+
+    /** Returns the area of this Circle instance */
+    public double getArea() {
+        return radius * radius * Math.PI;
+    }
+
+    // Getter for instance variable color
+    public String getColor() {
+        return color;
+    }
+
+    // Setter for instance variable radius
+    public void setRadius(double newRadius) {
+        radius = newRadius;
+    }
+
+    // Setter for instance variable color
+    public void setColor(String newColor) {
+        color = newColor;
+    }
+
+    /** Return a self-descriptive string of this instance in the form of Circle[radius=?,color=?] */
+    public String toString() {
+        return "Circle[radius=" + radius + " color=" + color + "]";
+    }
+}
