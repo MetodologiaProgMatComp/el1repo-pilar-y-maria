766cc596392cb2c8a6d90910d47a59288d2d58a7
mariadelaorden <mariadelaorden2005@hotmail.com>
2024-02-14 08:52:58 +0100

    Cambios

--- Ejercicio 1/Main.java
+++ Ejercicio 1/Main.java
@@ -2,6 +2,9 @@ package Ejercicio
 
 public class Main {
     public static void main(String[] args) {
+        Circle miObjeto=new Circle();
+        System.outprintln(miObjeto.toString());
+
 
     }
 }
