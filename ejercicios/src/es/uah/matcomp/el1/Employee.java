a6f25be84fb496143a4c99dfd3de8bca5df4d9dd
pilarpadrino <pilar.padrino@edu.uah.es>
2024-02-20 18:20:50 +0100

    ejercicio employee

---
+++ ejercicios/src/es/uah/matcomp/el1/Employee.java
@@ -0,0 +1,25 @@
+package es.uah.matcomp.el1;
+
+public class Employee {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int salary;
+    //CONSTRUCTOR
+    public Employee(int id, String firstName, String lastName, int salary){
+        this.id=id;
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.salary=salary;
+    }
+
+    public int getId(){return this.id; }
+    public String getFirstName(){return this.firstName; }
+    public String getName(){return "firstName lastname";}
+    public int getSalary(){return this.salary; }
+    public void getSalary(int salary){this.salary=salary; }
+    public int getAnnualSalary(){return salary*12; }
+    public int raiseSalary(int percent){return salary*percent; }
+    public String toString(){return "Employee[id"+id+"name="+firstName+lastName+",salary="+salary+"]"; }
+
+}
