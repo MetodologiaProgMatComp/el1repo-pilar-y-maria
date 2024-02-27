813cc56779adf112f48cfaad52f6ac5a7ed35114
mariadelaorden <mariadelaorden2005@hotmail.com>
2024-02-27 19:06:01 +0100

    test circle

--- ejercicios/Test/java/es/uah/matcomp/el1/CircleTest.java
+++ ejercicios/Test/java/es/uah/matcomp/el1/CircleTest.java
@@ -1,7 +1,38 @@
 package es.uah.matcomp.el1;
 
+import es.uah.matcomp.el1.p1.Circle;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CircleTest {
+    @org.junit.jupiter.api.Test
+    void getRadius(){
+        Circle c1= new Circle();
+        assertEquals(1.0d,c1.getRadius());
+        Circle c2= new Circle(2.0d);
+        assertEquals(2.0d,c2.getRadius());
+    }
+    @org.junit.jupiter.api.Test
+    void setRadius(){
+        Circle c1= new Circle(1.0d);
+        c1.setRadius(2.0d);
+        assertEquals(2.0d,c1.getRadius());
+    }
+    @org.junit.jupiter.api.Test
+    void getArea(){
+        Circle c1= new Circle(1.0d);
+        assertEquals(Math.PI,c1.getArea());
+    }
+    @org.junit.jupiter.api.Test
+    void getCircumference(){
+        Circle c1= new Circle(1.0d);
+        assertEquals(2*Math.PI,c1.getCircumference());
+    }
+    @org.junit.jupiter.api.Test
+    void testtoString(){
+        Circle c1= new Circle(1.0d);
+        assertEquals("Circle[radius="+1.0d+"]",c1.toString());
+    }
+
 
 }
