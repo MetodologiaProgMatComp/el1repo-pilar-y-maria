c288eee803028343094d1d07eced2ead241a3884
pilarpadrino <pilar.padrino@edu.uah.es>
2024-03-06 13:00:43 +0100

    útlimos arreglos

--- ejercicios/Test/java/es/uah/matcomp/el1/p4/DogTest.java
+++ ejercicios/Test/java/es/uah/matcomp/el1/p4/DogTest.java
@@ -8,13 +8,18 @@ class DogTest {
 
     @Test
     void greets() {
-        Dog a2 = new Dog( "Piñon");
-        a2.greets();
+        Dog a2 = new Dog( "Piñón");
+        assertEquals("Piñón", a2.getName());
     }
-
+    @Test
+    void greets1() {
+        Dog Dog1 = new Dog("Fuet");
+        assertEquals("Woof", Dog1.greets());
+    }
     @Test
     void testGreets() {
-        Dog a3 = new Dog("Fuet");
-        a3.greets();
+        Dog Dog2 = new Dog("Fuet");
+        Dog Dog3 = new Dog("Rex");
+        assertEquals("Woooof", Dog3.greets(Dog2));
     }
 }
